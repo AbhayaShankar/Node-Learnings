fc1fed1892ad258003541687f2dab299af91703d
Abhaya <shankarabhaya9@gmail.com>
2023-06-02 01:23:40 +0530

    readfileSync writeFileSync

--- 01 - First Node App/09-fs.js
+++ 01 - First Node App/09-fs.js
@@ -1,7 +1,28 @@
-const fs = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 const path = require("path");
 
 const filepath = path.join(process.cwd(), "context", "subfolder", "test.txt");
 
-const data = fs.readFileSync(filepath, "utf-8");
+const data = readFileSync(filepath, "utf-8");
 console.log(data);
+
+// reading and writing a file using node's built-in modules
+
+const firstPath = path.join(process.cwd(), "context", "first.txt");
+const secondPath = path.join(process.cwd(), "context", "second.txt");
+
+const firstFile = readFileSync(firstPath, "utf8");
+const secondFile = readFileSync(secondPath, "utf8");
+
+console.log(
+  "First File : ",
+  firstFile,
+  "and the",
+  "Second File : ",
+  secondFile
+);
+
+const result = writeFileSync(
+  "./context/resultingFile.txt",
+  `The Resulted File is : ${firstFile} ${secondFile}`
+);
