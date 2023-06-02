274408ff2d8591d54d430eed470d75a3232f52ac
Abhaya <shankarabhaya9@gmail.com>
2023-06-02 13:08:31 +0530

    http method

---
+++ 01 - First Node App/12-http.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" || req.url === "/about") {
+    if (req.url === "/") {
+      res.end("Hello World...Welcome to the Home Page");
+    } else if (req.url === "/about") {
+      res.end("This is the About Page");
+    }
+  } else {
+    res.end(`
+        <h1> Oops! Something went wrong! </h1>
+        <p> The page you are requesting doesn't exist. </p>
+        <a href="/">Back to home</a>
+      `);
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server listening on pport 5000");
+});
