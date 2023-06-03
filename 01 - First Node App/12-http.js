0c7ea9aa287ec0fd9bbe12c130392fbd2dedbd79
Abhaya <shankarabhaya9@gmail.com>
2023-06-03 16:59:50 +0530

    nodemon install

--- 01 - First Node App/12-http.js
+++ 01 - First Node App/12-http.js
@@ -17,5 +17,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(5000, () => {
-  console.log("Server listening on pport 5000");
+  console.log("Server listening on port 5000");
 });
