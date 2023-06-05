0452c1de452045ae053120f3c92f94ec49f1b9c9
Abhaya <shankarabhaya9@gmail.com>
2023-06-05 16:53:32 +0530

    products route, single product route using id

--- 03-express/app.js
+++ 03-express/app.js
@@ -4,7 +4,26 @@ const { products } = require("./data");
 const app = express();
 
 app.get("/", (req, res) => {
+  res.send(
+    '<h1>Home Page</h1> <a href="/api/allProducts">All Products</a>  <a href="/api/products">products</a>'
+  );
+});
+
+app.get("/api/allProducts", (req, res) => {
   res.json(products);
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((prod) => {
+    const { id, name, image, price } = prod;
+    return { id, name, image, price };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const singleProduct = products.find((prod) => +prod.id === +req.params.id);
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
