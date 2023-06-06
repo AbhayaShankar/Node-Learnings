565efd8e0c1d1bdb592a4fa9f65955235dab138d
Abhaya <shankarabhaya9@gmail.com>
2023-06-06 23:58:06 +0530

    refractor code

---
+++ 03-express/controllers/people.js
@@ -0,0 +1,68 @@
+let { people } = require("../data");
+
+const getPeople = (req, res) => {
+  res.status(200).json({ success: true, data: people });
+};
+
+const createPeople = (req, res) => {
+  console.log("Body", req.body);
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please enter name field" });
+  }
+  res.status(201).json({ success: true, person: name });
+};
+
+const createNewPeople = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please enter name field" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+};
+
+const updatePeople = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === +id);
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person found with id : ${id}` });
+  }
+  const newPeople = people.map((peep) => {
+    if (peep.id === +id) {
+      peep.name = name;
+    }
+    return peep;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+};
+
+const deletePeople = (req, res) => {
+  const { id } = req.params;
+  const person = people.find((person) => person.id === +id);
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id: ${id}` });
+  }
+  const newPeople = people.filter((peep) => peep.id !== +id);
+  console.log(newPeople);
+  return res.status(200).json({ success: true, data: newPeople });
+};
+
+module.exports = {
+  getPeople,
+  createPeople,
+  createNewPeople,
+  updatePeople,
+  deletePeople,
+};
--- 03-express/routes/people.js
+++ 03-express/routes/people.js
@@ -1,65 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-let { people } = require("../data");
+const {
+  getPeople,
+  createPeople,
+  createNewPeople,
+  updatePeople,
+  deletePeople,
+} = require("../controllers/people");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+router.get("/", getPeople);
 
-router.post("/", (req, res) => {
-  console.log("Body", req.body);
-  const { name } = req.body;
+router.post("/", createPeople);
 
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "Please enter name field" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
+router.post("/postman", createNewPeople);
 
-router.post("/postman", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "Please enter name field" });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
+router.put("/:id", updatePeople);
 
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === +id);
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `No person found with id : ${id}` });
-  }
-  const newPeople = people.map((peep) => {
-    if (peep.id === +id) {
-      peep.name = name;
-    }
-    return peep;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const person = people.find((person) => person.id === +id);
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id: ${id}` });
-  }
-  const newPeople = people.filter((peep) => peep.id !== +id);
-  console.log(newPeople);
-  return res.status(200).json({ success: true, data: newPeople });
-});
+router.delete("/:id", deletePeople);
 
 module.exports = router;
