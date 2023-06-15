15a6268797d4f628e4e3ae074d503d71ebfc22c6
Abhaya <shankarabhaya9@gmail.com>
2023-06-15 13:03:30 +0530

    connect to MongoDB

--- Store API/app.js
+++ Store API/app.js
@@ -2,24 +2,36 @@ require("dotenv").config();
 const NotFound = require("./middleware/not-found");
 const ErrorHandler = require("./middleware/error-handler");
 
+const connectDB = require("./db/connect");
+
 // Async errors
 
 const express = require("express");
-const errorHandlerMiddleware = require("./middleware/error-handler");
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
+//Routes
+
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Home Page" });
+  res.send(
+    `<h1>THE STORE API</h1> <a href="/api/v1/products">Products Page</a>`
+  );
+  //   res.status(200).json({ msg: "Home Page" });
 });
 
 app.use(NotFound);
-app.use(errorHandlerMiddleware);
+app.use(ErrorHandler);
 
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    app.listen(port, () => console.log(`The server is running on ${port}...`));
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`The server is listening on ${port}...`)
+    );
   } catch (error) {
     console.log(error);
   }
