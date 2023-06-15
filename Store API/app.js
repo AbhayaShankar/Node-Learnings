d68f165eb29dec7c8a1127e6ee02da844d6caaf7
Abhaya <shankarabhaya9@gmail.com>
2023-06-15 12:01:13 +0530

    basic route setup

--- Store API/app.js
+++ Store API/app.js
@@ -1,1 +1,28 @@
-console.log('04 Store API')
+require("dotenv").config();
+const NotFound = require("./middleware/not-found");
+const ErrorHandler = require("./middleware/error-handler");
+
+// Async errors
+
+const express = require("express");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Home Page" });
+});
+
+app.use(NotFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    app.listen(port, () => console.log(`The server is running on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
