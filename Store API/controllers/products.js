d1a25162e6c51bea2e8ab8920070bff20f49c092
Abhaya <shankarabhaya9@gmail.com>
2023-06-17 11:32:40 +0530

    dynamic find parameter

--- Store API/controllers/products.js
+++ Store API/controllers/products.js
@@ -4,11 +4,20 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ featured: true });
   //   throw new Error("Testing Async Route");
-  res.status(200).json({ msg: products, nbHits: products.length });
+  res.status(200).json({ prod: products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products Route" });
+  // const { featured } = req.query;
+  // console.log(featured);
+
+  const products = await Product.find(req.query);
+  //   const queryObject = {};
+
+  //   if (featured) {
+  //     queryObject.featured = featured;
+  //   }
+  res.status(200).json({ prod: products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
