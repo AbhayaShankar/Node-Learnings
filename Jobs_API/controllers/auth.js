2582aeda1265254a08867dfd5fdfe8a6da818d84
Abhaya <shankarabhaya9@gmail.com>
2023-06-20 11:26:55 +0530

    mongoose middlewares

--- Jobs_API/controllers/auth.js
+++ Jobs_API/controllers/auth.js
@@ -1,19 +1,13 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const user = await User.create({ ...req.body });
+  console.log(user);
+  const token = user.createJWT();
 
-  const salt = await bcrypt.genSalt(10);
-
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json(user);
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
--- Jobs_API/models/User.js
+++ Jobs_API/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 var validateEmail = function (email) {
   var re =
@@ -33,5 +35,16 @@ const UserSchema = new Schema({
     minlength: 6,
   },
 });
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "JWTSecretCode", {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
