0c6202e01724a9e38583464cbd531f2f431b380d
Abhaya <shankarabhaya9@gmail.com>
2023-06-22 01:49:22 +0530

    Jobs schema and CRUD fucntionality

--- Jobs_API/controllers/jobs.js
+++ Jobs_API/controllers/jobs.js
@@ -1,5 +1,10 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("Get All Jobs");
+  const allJobs = await Job.find({ createdBy: req.user.userId });
+  res.status(StatusCodes.OK).json(allJobs);
 };
 
 const getJob = async (req, res) => {
@@ -7,7 +12,10 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  // job = { ...job, job_id: job._id };
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 const updateJob = async (req, res) => {
--- Jobs_API/models/Job.js
+++ Jobs_API/models/Job.js
@@ -2,23 +2,31 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const JobSchema = new Schema({
-  company: {
-    type: String,
-    required: [true, "please provide company name"],
-    maxlength: 50,
-  },
+const JobSchema = new Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "please provide company name"],
+      maxlength: 50,
+    },
 
-  position: {
-    type: String,
-    required: [true, "Enter your position of responsibility"],
-    maxlength: 80,
+    position: {
+      type: String,
+      required: [true, "Enter your position of responsibility"],
+      maxlength: 80,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "pending", "declined"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide User"],
+    },
   },
-  status: {
-    type: String,
-    enum: ["interview", "pending", "declined"],
-    default: "pending",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Jobs", JobSchema);
