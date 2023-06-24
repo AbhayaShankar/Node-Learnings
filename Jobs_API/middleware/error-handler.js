c164a9fb02cc4ba00ae90bff497cf933957afba9
Abhaya <shankarabhaya9@gmail.com>
2023-06-24 13:30:43 +0530

    Error handlers

--- Jobs_API/middleware/error-handler.js
+++ Jobs_API/middleware/error-handler.js
@@ -1,10 +1,37 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    // Default error responses
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || `Something went wrong`,
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+
+  if (err.name === "ValidationError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors)
+      .map((errItem) => errItem.message)
+      .join(" and ");
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `Duplicate email provided for ${Object.keys(
+      err.keyValue
+    )}: ${Object.values(err.keyValue)}`;
+  }
+
+  if (err.name === "CastError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `No Job found for id : ${err.value}`;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
