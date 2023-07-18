8f51e5de31baab28d1f00336cf3f31b09f1ad914
Abhaya <shankarabhaya9@gmail.com>
2023-07-19 05:04:47 +0530

    cloudinary connect

--- File Upload/app.js
+++ File Upload/app.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 require("express-async-errors");
 const fileUpload = require("express-fileupload");
 
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const express = require("express");
 const app = express();
 
--- File Upload/controllers/uploadsController.js
+++ File Upload/controllers/uploadsController.js
@@ -1,12 +1,28 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 
+const CustomError = require("../errors");
+
 const uploadImageProduct = async (req, res) => {
-  console.log(req.files);
-  console.log("Name of the file : ", req.files.image.name);
-  console.log("mimetype : ", req.files.image.mimetype);
+  // console.log(req.files);
+  // console.log("Name of the file : ", req.files.image.name);
+  // console.log("mimetype : ", req.files.image.mimetype);
+
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
 
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please provide valid Image file");
+  }
+
+  if (productImage.size > 204800) {
+    throw new CustomError.BadRequestError(
+      "Please ensure image is smaller than 30KB"
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
