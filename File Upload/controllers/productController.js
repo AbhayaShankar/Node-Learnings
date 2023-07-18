03fecd074d51fdc3a2518605568ecedca811cfb2
Abhaya <shankarabhaya9@gmail.com>
2023-07-18 17:56:43 +0530

    making image static on servers

--- File Upload/controllers/productController.js
+++ File Upload/controllers/productController.js
@@ -2,7 +2,8 @@ const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 
 const createProduct = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ created: true });
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json(product);
 };
 
 const getAllProducts = async (req, res) => {
--- File Upload/controllers/uploadsController.js
+++ File Upload/controllers/uploadsController.js
@@ -1,7 +1,23 @@
+const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 
 const uploadImageProduct = async (req, res) => {
-  res.send("Upload Product Image");
+  console.log(req.files);
+  console.log("Name of the file : ", req.files.image.name);
+  console.log("mimetype : ", req.files.image.mimetype);
+
+  const productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadImageProduct };
