4935bdc3a1fec421a4a75c6ad8bc1ac674abf970
Abhaya <shankarabhaya9@gmail.com>
2023-07-19 17:10:38 +0530

    storing images on cloudinary

--- File Upload/controllers/uploadsController.js
+++ File Upload/controllers/uploadsController.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
-
+const cloudinary = require("cloudinary").v2;
 const CustomError = require("../errors");
+const fs = require("fs");
 
-const uploadImageProduct = async (req, res) => {
+const uploadImageProductLocal = async (req, res) => {
   // console.log(req.files);
   // console.log("Name of the file : ", req.files.image.name);
   // console.log("mimetype : ", req.files.image.mimetype);
@@ -34,6 +35,23 @@ const uploadImageProduct = async (req, res) => {
   res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadImageProduct = async (req, res) => {
+  // console.log(req.files.image);
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload-proj",
+    }
+  );
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+
+  // console.log(result);
 };
 
 module.exports = { uploadImageProduct };
