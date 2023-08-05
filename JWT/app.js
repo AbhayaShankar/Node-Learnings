a97eac938aab41da0011fa48c9572133f663d376
Abhaya <shankarabhaya9@gmail.com>
2023-08-05 12:02:57 +0530

    setting up middlewares

--- JWT/app.js
+++ JWT/app.js
@@ -1,5 +1,9 @@
+// used for incorporating .env variables into your project.
 require("dotenv").config();
+
+// creating custom error handlers
 require("express-async-errors");
+
 const UserRouter = require("./routes/main");
 
 const express = require("express");
@@ -8,8 +12,12 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// middleware
+// Middlewares
+
+// this is used to statically geenrate your public build file - similar to next js static geenration. But here the index.html file will be served statically for server side.
 app.use(express.static("./public"));
+
+// this is used so that we are able to view req.body object when a post route is being hit.
 app.use(express.json());
 
 app.use("/api/v1", UserRouter);
