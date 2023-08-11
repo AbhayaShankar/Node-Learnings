0929da1b257c8ec9aeae31063fc3f64cd963a268
Abhaya <shankarabhaya9@gmail.com>
2023-08-11 12:34:18 +0530

    file upload

--- File Upload/app.js
+++ File Upload/app.js
@@ -37,7 +37,7 @@ app.use("/api/v1/products", ProductRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
--- File Upload/routes/productRoutes.js
+++ File Upload/routes/productRoutes.js
@@ -9,8 +9,11 @@ const {
 
 const router = express.Router();
 
-router.post("/", createProduct);
-router.get("/", getAllProducts);
+// router.post("/", createProduct);
+// router.get("/", getAllProducts);
+
+// Above two lines of code are same but below is chained and above is explicit.
+router.route("/").get(getAllProducts).post(createProduct);
 router.post("/uploads", uploadImageProduct);
 
 module.exports = router;
