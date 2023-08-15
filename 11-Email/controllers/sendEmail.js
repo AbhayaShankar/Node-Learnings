178e0501d637b06bdd58357f0b331aaba0f2ae16
Abhaya <shankarabhaya9@gmail.com>
2023-08-15 22:57:11 +0530

    Nodemailer and Ethereal Setup

--- 11-Email/controllers/sendEmail.js
+++ 11-Email/controllers/sendEmail.js
@@ -1,5 +1,24 @@
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (req, res) => {
-  res.status(200).json({ msg: "Success" });
+  const transporter = await nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Abhaya Shankar" <shankarabhaya9@gmail.com>',
+    to: "abhayacodes@gmail.com ",
+    subject: "Testing Emails",
+    text: "Testing Emails with Ethereal",
+    html: "<h2> This is Working... </h2>",
+  });
+
+  res.status(200).json({ info });
 };
 
 module.exports = sendEmail;
